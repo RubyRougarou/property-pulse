1ed99862c589380d67ea13dc0a5c74f7088a53b9
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2025-01-31 15:44:42 +0330

    implementing HomeProperties.

---
+++ src/app/_components/HomeProperties.js
@@ -0,0 +1,5 @@
+const HomeProperties = () => {
+  return <div>Some home cards...</div>;
+};
+
+export default HomeProperties;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/app/_components/Hero";
 import InfoBoxes from "@/app/_components/InfoBoxes";
+import HomeProperties from "@/app/_components/HomeProperties";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 }
