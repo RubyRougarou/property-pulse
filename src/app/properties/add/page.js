bde38cbe32ee686e128bf7d2e8f024054d2432ef
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2025-02-09 14:16:18 +0330

    implemented log out, authentication middleware and add property form.

--- src/app/properties/add/page.js
+++ src/app/properties/add/page.js
@@ -1,14 +1,14 @@
 import Link from "next/link";
+import AddPropertyForm from "@/app/_components/AddPropertyForm";
 
 export default function AddPropertyPage() {
   return (
-    <div>
-      Add a new property
-      <div>
-        <Link href={"/"} className={"m-8 text-blue-500"}>
-          Back to Home Page
-        </Link>
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-2xl py-8">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <AddPropertyForm />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
