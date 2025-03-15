3726ae7a1b8e616e3570484681ead5c5f203bd2e
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2025-03-15 13:41:39 +0330

    refactored properties page.

--- src/app/properties/page.js
+++ src/app/properties/page.js
@@ -1,12 +1,7 @@
-import PropertyCard from "@/app/_components/PropertyCard";
-import { getProperties } from "@/app/_libs/requests";
 import SearchPropertyForm from "@/app/_components/SearchPropertyForm";
+import Properties from "@/app/_components/Properties";
 
-export default async function PropertiesPage() {
-  const properties = await getProperties();
-
-  // Sort properties by date
-  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+export default function PropertiesPage() {
   return (
     <>
       <section className="bg-blue-700 py-4">
@@ -14,19 +9,7 @@ export default async function PropertiesPage() {
           <SearchPropertyForm />
         </div>
       </section>
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties?.length === 0 ? (
-            <p>No properties found...</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties?.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 }
